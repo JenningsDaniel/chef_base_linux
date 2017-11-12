669a09f112ceca3c03a6b220f074df51bf4c3056
Daniel Jennings <daniel.j.jennings@gmail.com>
2017-11-11 21:39:31 -0500

    updated for comply with food critic

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'chef_base_linux'
 maintainer 'Daniel Jennings'
 maintainer_email 'daniel.j.jennings@gmail.com'
-license 'All Rights Reserved'
+license 'Apache-2.0'
 description 'Applies common linux packages and configuration'
 long_description 'Applies common linux packages and configuration'
 version '1.0.0'
@@ -9,15 +9,7 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 depends 'vim'
 depends 'git'
 depends 'pathogen'
+supports 'ubuntu'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/chef_base_linux/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/chef_base_linux'
+issues_url 'https://github.com/JenningsDaniel/chef_base_linux/issues'
+source_url 'https://github.com/JenningsDaniel/chef_base_linux'
